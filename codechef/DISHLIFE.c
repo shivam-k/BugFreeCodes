90cd0aa3a10ef54be169bd95925352b0044ff823
shivam-k <shivamkumar30013@gmail.com>
2017-04-09 17:44:56 +0530

    --Incomplete

---
+++ codechef/DISHLIFE.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+	int T;
+	scanf("%d", &T);
+	while(T--)
+	{
+		int n, k, i;
+		scanf("%d %d", &n, &k);
+		for(i=0; i<n; i++)
+		{
+			
+		}
+	}	
+	
+	return 0;
+}
