3e5d0c969e9a602b90fbc8e9cc40a801404f9d4c
shivam-k <shivamkumar30013@gmail.com>
2017-04-10 12:41:13 +0530

    WA

--- codechef/DISHLIFE.c
+++ codechef/DISHLIFE.c
@@ -3,19 +3,51 @@
 #include <math.h>
 #include <string.h>
 
+
 int main(void)
 {
 	int T;
 	scanf("%d", &T);
 	while(T--)
 	{
-		int n, k, i;
+		int arr[100002]={0};
+		int n, k, j, count=0; 
 		scanf("%d %d", &n, &k);
-		for(i=0; i<n; i++)
+
+		for(j=0; j<n; j++) //islands 
 		{
-			
+			int p, i, pass=0;
+			scanf("%d", &p); //ingredients
+			for (i=0; i<p; ++i)
+			{
+				int num;
+				scanf("%d", &num);
+				if(num<=k && num>=1 && arr[num-1]==0)
+				{
+					arr[num-1]=num;
+					count++;
+				}
+				if(count==k)
+				{
+					pass=1; break;
+				}
+			}
+			printf("%d\n", count);
+			if(count==k && pass==1)
+				break;
 		}
-	}	
-	
+
+		for(int mn=0; mn<k; mn++)
+			printf("%d ", arr[mn]);
+		printf("\n");
+		
+		if(count==k && j==n-1)
+			printf("all\n");	
+		else if(count<k)
+			printf("sad\n");
+		else if(count==k && j<n-1)
+			printf("some\n");
+	}
+
 	return 0;
 }
