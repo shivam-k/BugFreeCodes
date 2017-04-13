dec87010ce24da15be10f9baabdda1ea0833ab54
shivam-k <shivamkumar30013@gmail.com>
2017-04-13 20:11:34 +0530

    --TLE

--- codechef/ROWSOLD.c
+++ codechef/ROWSOLD.c
@@ -16,34 +16,36 @@ int main(void)
 		if(len==1)
 		{
 			printf("0\n");
-			break;
 		}
-		while(1)
+		else
 		{
-			if(i==0)
-				pass=0;
-			if(str[i]=='1' && str[i+1]=='0')
+			while(1)
 			{
-				count++;
-				while(str[i+1]=='0')
+				if(i==0)
+					pass=0;
+				if(str[i]=='1' && str[i+1]=='0')
 				{
-					str[i+1]='1';
-					str[i]='0';
-					count++; i++;
-					pass=1;
+					count++;
+					while(str[i+1]=='0')
+					{
+						str[i+1]='1';
+						str[i]='0';
+						count++; i++;
+						pass=1;
+					}
+				}
+				else
+					i++;
+				if(i==len-1)
+				{
+					if(pass==0)
+						break;
+					else
+						i=0;
 				}
 			}
-			else
-				i++;
-			if(i==len-1)
-			{
-				if(pass==0)
-					break;
-				else
-					i=0;
-			}
+			printf("%d\n", count);
 		}
-		printf("%d\n", count);
 
 	}
 
