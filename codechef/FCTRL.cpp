a3a0ab0d24fa4cffba85290c5dccd57d436e799b
shivam-k <shivamkumar30013@gmail.com>
2017-04-18 18:37:40 +0530

    Tue Apr 18 18:37:40 IST 2017

---
+++ codechef/FCTRL.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib> 
+#include <cmath>
+using namespace std;
+
+
+int main(void)
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		unsigned long long int n, count=0, temp, i;
+		cin>>n;
+		
+		i=1;
+
+		while(pow(5, i)<=n)
+		{
+			count=count+(n/pow(5, i));
+			i++;
+		}
+
+		cout<<count<<"\n";
+		
+	}
+
+	return 0;
+}
+
+
