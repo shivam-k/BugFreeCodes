dd0dd2b4d5825caa21f71b6c0bbb117f9396ce59
shivam-k <shivamkumar30013@gmail.com>
2017-05-02 11:44:23 +0530

    Tue May  2 11:44:23 IST 2017

---
+++ gforg/BST-Preorder-Traversal.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node
+{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+void printlist(node *head)
+{
+	if(head==NULL)
+		return;
+	else
+	{
+		printf("%d ", head->data);
+		printlist(head->left);
+		printlist(head->right);
+	}
+}
+
+int main(void)
+{
+	node *root, *a, *b, *c, *d, *e;
+
+	a=(node*)malloc(sizeof(node));
+	b=(node*)malloc(sizeof(node));
+	c=(node*)malloc(sizeof(node));
+	d=(node*)malloc(sizeof(node));
+	e=(node*)malloc(sizeof(node));
+	
+	a->data=1;
+	b->data=2;
+	c->data=3;
+	d->data=4;
+	e->data=5;
+
+	a->left=b;
+	a->right=c;
+	
+	b->left=d;
+	b->right=e;
+
+	c->left=NULL;
+	c->right=NULL;
+
+	d->left=NULL;
+	d->right=NULL;
+
+	e->left=NULL;
+	e->right=NULL;
+
+	root=a;
+	printlist(root);
+
+	printf("\n");
+
+	return 0;
+}
+
+
