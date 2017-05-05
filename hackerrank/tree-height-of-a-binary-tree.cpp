478b213332a400ca13999f0ee6f86ea0de1ae398
shivam-k <shivamkumar30013@gmail.com>
2017-05-06 05:05:35 +0530

    Sat May  6 05:05:34 IST 2017

---
+++ hackerrank/tree-height-of-a-binary-tree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        int a=0, b=0;
+        if(root==NULL)
+            return -1;
+        else
+            {
+            a=height(root->left);
+            b=height(root->right);
+            if(b>a)
+                a=b;
+            return ++a;
+        }
+    }
+  }; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.height(root);
+    cout << height<<"\n";
+
+    return 0;
+}
